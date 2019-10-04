a92be81304a531398dd15bf83d496cc3878c35f5
agumabong <32043546+agumabong@users.noreply.github.com>
2019-10-03 17:06:29 -0700

    Add files via upload

---
+++ CPSC 481 Distance Project.py
@@ -0,0 +1,27 @@
+#Variables to track if the user wants to find distance or time
+distance = False
+time = False
+
+#ask the user whether or not they want to find shortest distance or time
+user_input = input("Sup, find the shortest distance or time?")
+if (user_input.lower() == "distance"):
+    distance = True
+elif (user_input.lower() == "time"):
+    time = True
+else:
+    print("invalid input, please put time or distance")
+    
+#preliminary starting location
+destination = input("Enter starting location (Format is City, State)")
+print("Location sent: " + destination)
+cont = input("Add another location: ")
+
+#loop until the user has no more locations
+route_finished = False
+while (route_finished == False):    
+    cont = input("Add another location: (enter Q to quit)")
+    if (cont.lower() == "q"):
+        route_finished= True
+    else:
+        print("location added was: " + cont)
+        #send info to the google API
